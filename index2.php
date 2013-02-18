646b58aa76704fd5eebe5b0e118d745478d26236
Mike Wilcox <mwilcox56@gmail.com>
2013-02-18 16:56:40 -0500

    updated main page and css

---
+++ index2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" name="viewport">
+   <meta name="author" content="Mike Wilcox">
+   <link href='http://fonts.googleapis.com/css?family=Fredericka+the+Great' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" type="text/css" href="css/normalize.css">
+   <link rel="stylesheet" type="text/css" href="css/bstrap.css">   
+   <link rel="stylesheet" type="text/css" href="css/bstrap-responsive.css">
+   <link rel="stylesheet" type="text/css" href="css/bootswatch.css">
+   <link rel="stylesheet" href="webfonts/ss-social.css" type="text/css" />
+</head>
+<body class="preview" id="top" data-spy="scroll" data-target=".subnav" data-offset="80">
+ <div class="navbar navbar-fixed-top">
+   <div class="navbar-inner">
+     <div class="container">
+       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </a>
+       <a class="brand" href="#">Mike Wilcox</a>
+       <div class="nav-collapse collapse" id="main-menu">
+        <ul class="nav social" id="main-menu-left">
+          <li><a onclick="pageTracker._link(this.href); return false;" href="http://news.bootswatch.com">ABOUT</a></li>
+          <li><a id="swatch-link" href="../#gallery">BLOG</a></li>
+	  <li><a id="swatch-link" href="../#gallery">RESUME</a></li>			
+	  <li style="padding-left:130px;"><a class="ss-icon ss-social-circle" href="https://github.com/mjw56">github</a></li>
+	  <li><a class="ss-icon ss-social-circle" href="https://twitter.com/mikeWilc0x">twitter</a></li>
+	  <li><a class="ss-icon ss-social-circle" href="mailto:mwilcox56@gmail.com">email</a></li>
+        </ul>
+        <ul class="nav pull-right" id="main-menu-right">
+        </ul>
+       </div>
+     </div>
+   </div>
+ </div>
+</body>
+</html>
